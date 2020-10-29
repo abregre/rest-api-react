e3f1a5a2688a822eaf43b9e1bce0268054cc868a
abregre <nonosgfour@gmail.com>
2020-10-29 22:13:44 +0200

    Article component

--- client/src/components/articles.js
+++ client/src/components/articles.js
@@ -1,11 +1,20 @@
 import './articles.css';
+import React, { useState, useEffect } from 'react';
+import axios from '../axios';
 
 function Articles() {
-
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get('/api/articles');
+      setArticles(req.data);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
-        <h2>Articles</h2>
+      <h2>Articles</h2>
     </div>
   );
 }
